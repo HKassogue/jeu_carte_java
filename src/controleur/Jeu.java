163304b1e4e26a8b24bac7f2a1136dab5ae85c34
Hamidou KASSOGUE <hamidoukass@gmail.com>
2021-09-30 18:59:52 +0000

    extraction d'evaluation du gagnant comme classe

--- src/controleur/Jeu.java
+++ src/controleur/Jeu.java
@@ -98,29 +98,8 @@ public class Jeu {
         } 
     }
     
-    void evaluerGagnant() { 
-        gagnant = null; 
-        int meilleurValeur = -1; 
-        int meilleurType = -1; 
-        for (Joueur joueur : joueurs) { 
-            boolean newGagnant = false; 
-            if (gagnant == null) newGagnant = true; 
-            else { 
-                Carte carte = joueur.getCarte(0); 
-                int valeur = carte.getValeur().getValeur(); 
-                if (valeur > meilleurValeur) newGagnant = true; 
-                else if(valeur == meilleurValeur) { 
-                    if (carte.getType().getType() > meilleurType) 
-                        newGagnant = true; 
-                }
-            }
-            if (newGagnant) { 
-                gagnant = joueur; 
-                Carte carte = joueur.getCarte(0); 
-                meilleurValeur = carte.getValeur().getValeur(); 
-                meilleurType = carte.getType().getType(); 
-            } 
-        } 
+    public void evaluerGagnant() {
+        gagnant = new Evaluation().evaluerGagnant(joueurs);
     }
     
     public void afficherGagnant() {
