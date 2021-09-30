36f17e9869eaf6de413e8036c2ff00bc99134330
Hamidou KASSOGUE <hamidoukass@gmail.com>
2021-09-30 00:21:06 +0000

    ajout de joueur

---
+++ src/model/Joueur.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author HKass
+ */
+public class Joueur {
+    private String nom;
+    private Main main;
+
+    public Joueur(String nom) {
+        this.nom = nom;
+        this.main = new Main();
+    }
+    
+    public void ajouterCarteMain(Carte c){
+        main.ajouterCarte(c);
+    }
+    
+    public Carte getCarte(int pos){
+        return main.getCarte(pos);
+    }
+    
+    public Carte retirerCarte(){
+        return main.retirerCarte();
+    }
+
+    public String getNom() {
+        return nom;
+    }
+    
+}
